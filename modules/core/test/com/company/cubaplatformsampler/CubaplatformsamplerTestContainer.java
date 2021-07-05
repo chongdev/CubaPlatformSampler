998bec0f0db3ae0bd28685b770c0a0d9f2c6e5c0
Chong <monkey.d.chong@gmail.com>
2021-07-05 17:41:44 +0700

    first commit

---
+++ modules/core/test/com/company/cubaplatformsampler/CubaplatformsamplerTestContainer.java
@@ -0,0 +1,58 @@
+package com.company.cubaplatformsampler;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CubaplatformsamplerTestContainer extends TestContainer {
+
+    public CubaplatformsamplerTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/cubaplatformsampler/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/cubaplatformsampler/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends CubaplatformsamplerTestContainer {
+
+        public static final CubaplatformsamplerTestContainer.Common INSTANCE = new CubaplatformsamplerTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}

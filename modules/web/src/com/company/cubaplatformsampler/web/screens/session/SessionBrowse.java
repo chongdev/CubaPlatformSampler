4446f19b7c6525bc992e171613f21673f283566f
Chong <monkey.d.chong@gmail.com>
2021-07-06 13:25:44 +0700

    ep2 - Using Screen API

--- modules/web/src/com/company/cubaplatformsampler/web/screens/session/SessionBrowse.java
+++ modules/web/src/com/company/cubaplatformsampler/web/screens/session/SessionBrowse.java
@@ -1,10 +1,12 @@
 package com.company.cubaplatformsampler.web.screens.session;
 
-import com.haulmont.cuba.gui.components.VBoxLayout;
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.components.Calendar;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.cubaplatformsampler.entity.Session;
 
 import javax.inject.Inject;
+import java.util.Date;
 
 @UiController("cubaplatformsampler_Session.browse")
 @UiDescriptor("session-browse.xml")
@@ -12,5 +14,17 @@ import javax.inject.Inject;
 @LoadDataBeforeShow
 public class SessionBrowse extends StandardLookup<Session> {
     @Inject
-    private VBoxLayout tab;
+    private ScreenBuilders screenBuilders;
+
+    @Subscribe("sessionsCalendar")
+    public void onSessionsCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<Date> event) {
+         Screen screen = screenBuilders.editor(Session.class, this).editEntity((Session) event.getEntity()).withLaunchMode(OpenMode.DIALOG).build();
+
+         screen.addAfterCloseListener(afterCloseEvent -> {
+             getScreenData().loadAll();
+         });
+
+         screen.show();
+    }
+    
 }

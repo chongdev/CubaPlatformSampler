40941014c13ed46e34fd41ad4c1c2d6a01d0f0fb
Chong <monkey.d.chong@gmail.com>
2021-07-10 01:20:49 +0700

    ep5 - TextField & passwordField
    Text Change Event Mode

--- modules/web/src/com/company/cubaplatformsampler/web/screens/login/LoginScreen.java
+++ modules/web/src/com/company/cubaplatformsampler/web/screens/login/LoginScreen.java
@@ -2,16 +2,16 @@ package com.company.cubaplatformsampler.web.screens.login;
 
 import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.cuba.gui.Notifications;
-import com.haulmont.cuba.gui.components.Button;
-import com.haulmont.cuba.gui.components.HasValue;
-import com.haulmont.cuba.gui.components.TextField;
-import com.haulmont.cuba.gui.components.TextInputField;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.screen.Screen;
 import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @UiController("cubaplatformsampler_LoginScreen")
 @UiDescriptor("login-screen.xml")
@@ -27,13 +27,43 @@ public class LoginScreen extends Screen {
     private TextField<String> textField;
     @Inject
     private Notifications notifications;
+    @Inject
+    private Label<String> textFieldLabel;
 
     boolean isClear = true;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+
+//        Map<String, TextChangeEventMode> modes = new HashMap<>();
+//        Arrays.stream(TextChangeEventMode.values()).forEach(mode -> modes.put(mode.toString(), mode));
+
+        textField.setCursorPosition(0);
+
+//        textField.addTextChangeListener(e->{
+//            int length = e.getText().length();
+//
+//            textFieldLabel.setValue(length + " of " + textField.getMaxLength());
+//        });
+//
+//        textField.addValueChangeListener(stringValueChangeEvent ->
+//            notifications.create()
+//                    .withCaption("Before: " + stringValueChangeEvent.getPrevValue() +
+//                            ". After: " + stringValueChangeEvent.getValue())
+//                    .show());
+
+        textField.addValueChangeListener(stringValueChangeEvent -> {
+
+        });
+    }
+
+
 
     @Subscribe("textField")
     public void onTextFieldValueChange(HasValue.ValueChangeEvent<String> event) {
         // TODO TextField Event เมื่อมีข้อมูลเปลี่ยน
-        btnClear.setEnabled(true);
+//        btnClear.setEnabled(true);
+
     }
     @Subscribe("textField")
     public void onTextFieldEnterPress(TextInputField.EnterPressEvent event) {
@@ -43,7 +73,17 @@ public class LoginScreen extends Screen {
     @Subscribe("textField")
     public void onTextFieldTextChange(TextInputField.TextChangeEvent event) {
         // TODO TextField Event เมื่อมีข้อมูลเปลี่ยน
-        btnSubmit.setEnabled(true);
+
+        Integer length = event.getText().length();
+        textFieldLabel.setValue( "_" + length );
+
+        if( length > 0 ){
+            btnSubmit.setEnabled(true);
+        }
+        else{
+            btnSubmit.setEnabled(false);
+        }
+
     }
 
     @Subscribe("btnSubmit")
